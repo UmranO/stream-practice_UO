23d3b7a5e40d6a77c5fe31dba7d4ff34523cf9c0
Umran Ozman <umonal@yahoo.com>
2024-10-28 18:31:18 -0400

    StreamPracticeApplication class updated

--- src/main/java/com/cydeo/streampractice/service/StreamPracticeApplication.java
+++ src/main/java/com/cydeo/streampractice/service/StreamPracticeApplication.java
@@ -1,4 +1,13 @@
 package com.cydeo.streampractice.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StreamPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamPracticeApplication.class, args);
+    }
+
 }
